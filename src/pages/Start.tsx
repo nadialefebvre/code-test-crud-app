7a99562a98a1487a2d9b37ecd3822e66a03ad255
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-10-08 20:31:26 +0200

    feat: add new components to Start

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react"
 import Grid from "@mui/material/Grid"
 
 import { API_ENDPOINT } from "../utils/urls"
+import Header from "../components/Header"
+import EndpointRequest from "../components/EndpointRequest"
+import Footer from "../components/Footer"
 
 const Start = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [actions, setActions] = useState([])
+  const [isEndpointExpired, setIsEndpointExpired] = useState(false)
 
   // ----- FETCH ITEMS -----
   useEffect(() => {
@@ -22,22 +26,33 @@ const Start = () => {
       .then((res) => res.json())
       .then((data) => {
         setActions(data)
-        console.log(actions)
         setIsLoading(false)
+        setIsEndpointExpired(false)
       })
       .catch((err) => {
         console.error("Error in App: ", err)
         setIsLoading(false)
+        setIsEndpointExpired(true)
       })
   }, [])
   // ---------------
+  console.log(actions)
 
   if (isLoading) {
     return <div>Loading...</div>
   }
+
   return (
     <Grid container spacing={4} sx={{ justifyContent: "space-around" }}>
-      <div>Test</div>
+      <Header />
+      {isEndpointExpired ? (
+        <EndpointRequest />
+      ) : (
+        <>
+          <div>list here</div>
+        </>
+      )}
+      <Footer />
     </Grid>
   )
 }
