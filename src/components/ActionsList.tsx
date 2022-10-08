982994c02ba7be5b0a36047b3329e946f9c9fec3
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-10-08 20:41:42 +0200

    feat: add ActionsList component

---
+++ src/components/ActionsList.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react"
+
+import Chip from "@mui/material/Chip"
+import Grid from "@mui/material/Grid"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+
+import SingleAction from "./SingleAction"
+
+import { feelings } from "../utils/feelings"
+
+import { actionsJSON } from "../types/types"
+
+interface Props {
+  actions: actionsJSON
+  fetchActions: () => void
+  isLoading: boolean
+}
+
+const ActionsList = ({ actions, fetchActions, isLoading }: Props) => {
+  useEffect(() => fetchActions(), [fetchActions])
+
+  const [inputSearch, setInputSearch] = useState<string[]>(feelings)
+
+  const filteredActions = actions.filter((item) =>
+    inputSearch.includes(item.feeling)
+  )
+
+  const toggleSearch = (feeling: string) => {
+    if (inputSearch.includes(feeling)) {
+      setInputSearch((arr) => arr.filter((item) => item !== feeling))
+    } else {
+      setInputSearch([...inputSearch, feeling])
+    }
+  }
+
+  return (
+    <>
+      {actions.length !== 0 && (
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={4} sx={{ justifyContent: "center" }}>
+            {feelings.map((item) => (
+              <Chip
+                key={item}
+                label={item}
+                color="secondary"
+                variant={inputSearch.includes(item) ? "filled" : "outlined"}
+                onClick={() => toggleSearch(item)}
+              />
+            ))}
+          </Stack>
+        </Grid>
+      )}
+      {isLoading ? (
+        <Typography variant="body1" color="text.secondary">
+          Loading...
+        </Typography>
+      ) : (
+        (inputSearch.length === 0 ? actions : filteredActions)
+          .map((item) => (
+            <SingleAction
+              key={item._id}
+              fetchActions={fetchActions}
+              isLoading={isLoading}
+              action={item}
+            />
+          ))
+          .reverse()
+      )}
+    </>
+  )
+}
+
+export default ActionsList
