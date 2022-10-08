e77f19ccca14f4edaada1809c88227424db370de
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-10-08 20:31:03 +0200

    feat: create EndpointRequest component

---
+++ src/components/EndpointRequest.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+import Grid from "@mui/material/Grid"
+import Card from "@mui/material/Card"
+import Link from "@mui/material/Link"
+import CardContent from "@mui/material/CardContent"
+import Typography from "@mui/material/Typography"
+
+const EndpointRequest = () => {
+  return (
+    <Grid item xs={12}>
+      <Card
+        sx={{
+          minWidth: 275,
+          backgroundColor: "#cbdde6",
+        }}
+      >
+        <CardContent>
+          <Typography sx={{ textAlign: "justify" }} variant="body1">
+            If you see this message, it means that the temparary endpoint this
+            little app needs is now expired (each endpoint last 24 hrs or 100
+            requests). If you{" "}
+            <Link href="https://crudcrud.com">click here</Link>, you can get a
+            new one. The endpoint identifier (the part following{" "}
+            <q>https://crudcrud.com/api/</q>) can be easily copied and pasted
+            after clicking on <q>Check Endpoint Information</q>. Change the
+            value of the <q>endpointIdentifier</q> variable in the code
+            (src/utils/urls.ts) to start over with a new enpoint.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
+
+export default EndpointRequest
