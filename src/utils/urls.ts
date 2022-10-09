4526877d387b71c6278901b7333cda5304c93db5
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-10-09 11:08:14 +0200

    fix: add missing endpointIdentifier

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,6 @@
 // This variable is the one to change every 24hrs
 // Pick the identifier here: https://crudcrud.com
-export const endpointIdentifier = ""
+export const endpointIdentifier = "24107d677ecf4f2e87e7850d00185b40"
 
 const BASE_URL = `https://crudcrud.com/api/${endpointIdentifier}`
 
