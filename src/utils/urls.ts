0d3904e34f6aa7bdd00d647df28fb07adc0451fc
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-10-09 09:50:52 +0200

    refactor: add export to variable

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,6 @@
 // This variable is the one to change every 24hrs
 // Pick the identifier here: https://crudcrud.com
-const endpointIdentifier = "9e2f44e810004787a6aa9ed3280a43ae"
+export const endpointIdentifier = "9e2f44e810004787a6aa9ed3280a43ae"
 
 const BASE_URL = `https://crudcrud.com/api/${endpointIdentifier}`
 
